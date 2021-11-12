856fa802d8d16fb160ee3ee23b5a7d8dfc55ce29
pablo44445 <p.gajado04@ufromail.cl>
2021-11-12 18:20:12 -0300

    Modificacion 1 de maestro - test

--- src/main/java/Gestionar_notas.java
+++ src/main/java/Gestionar_notas.java
@@ -2,7 +2,5 @@ import java.util.ArrayList;
 
 public interface Gestionar_notas {
     public void verNotas();
-    public boolean ingresarNotas();
-    public void modificarNotas();
     public double calcularNotaNecesaria();
 }
