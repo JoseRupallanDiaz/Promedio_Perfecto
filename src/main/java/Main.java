fd2afa848900b51fa9e92d3400be48c7de03a40a
pablo44445 <p.gajado04@ufromail.cl>
2021-12-06 17:14:39 -0300

    Correccion de bugs itineraciones. 0.1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Log log;
-        log = new Log("logs.lck");
+        log = new Log();
 
         String nombre = "Diego";
         log.addLine("ingreso "+nombre);
